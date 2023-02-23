4230b419252f5b743043c58080b3a24009a16285
Alija Suljic <fhs47796@fh-salzburg.ac.at>
2023-02-23 19:08:04 +0100

    Added work sample for story of soell

---
+++ C#/StoryOfSoell/Playermovement.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playermovement : MonoBehaviour
+{
+	public CharacterController2D controller;
+	public Animator animator;
+	public GameObject gameoverText, Restartbutton, timer, pause;
+	public float runSpeed = 40f;
+	
+	float horizontalMove = 0f;
+	bool jump = false;
+	bool crouch = false;
+	
+    void Start()
+    {
+        gameoverText.SetActive(false);
+		Restartbutton.SetActive(false);
+    }
+
+    void Update()
+    {
+		horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+		
+		animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+		
+		if (Input.GetButtonDown("Jump"))
+		{
+			jump = true;
+			FindObjectOfType<AudioManager>().Play("Jump");
+			animator.SetBool("IsJumping", true);
+		}
+		
+		if (Input.GetButtonDown("Crouch"))
+		{
+			crouch = true;
+		} 
+		else if (Input.GetButtonUp("Crouch"))
+		{
+			crouch = false;
+		}
+    }
+	
+	public void OnLanding ()
+	{
+		animator.SetBool("IsJumping", false);
+	}
+	
+	public void OnCrouching (bool isCrouching)
+	{
+		animator.SetBool("IsCrouching", isCrouching);
+	}
+	
+	void FixedUpdate ()
+	{
+		controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+		jump = false;
+	}
+	
+	void OnCollisionEnter2D (Collision2D col)
+	{
+		if (col.gameObject.tag.Equals("Enemy"))
+		{
+			FindObjectOfType<AudioManager>().Play("OwlDeath");
+			Destroy(timer);
+			Destroy(pause);
+			Time.timeScale = 1f;
+			gameoverText.SetActive(true);
+			Restartbutton.SetActive(true);
+			gameObject.SetActive(false);
+		}
+	}
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.gameObject.CompareTag("Worms"))
+		{
+			FindObjectOfType<AudioManager>().Play("Food");
+			Destroy(other.gameObject);
+		}
+	}
+	
+}
---
+++ C#/StoryOfSoell/enemy_movement.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemy_movement : MonoBehaviour 
+{
+    public float walkSpeed = 1.0f;      
+    public float wallLeft = 0.0f;       
+    public float wallRight = 5.0f;      
+    float walkingDirection = 1.0f;
+    Vector2 walkAmount;
+    float originalX;
+	private bool movingRight = true;
+
+    void Start () 
+    {
+        this.originalX = this.transform.position.x;
+		wallLeft = transform.position.x - 4f;
+		wallRight = transform.position.x + 4f;
+    }
+
+    void Update () 
+    {
+        walkAmount.x = walkingDirection * walkSpeed * Time.deltaTime;
+        if (walkingDirection > 0.0f && transform.position.x >= wallRight) 
+		{
+            walkingDirection = -1.0f;
+			transform.localScale= new Vector2(2,2);
+			movingRight = false;
+        } 
+		else if (walkingDirection < 0.0f && transform.position.x <= wallLeft) 
+		{
+            walkingDirection = 1.0f;
+			transform.localScale= new Vector2(-2,2);
+			movingRight = true;
+        }
+        transform.Translate(walkAmount);
+    }
+}
